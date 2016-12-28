1e1086129d77d97ce25a49ae284b6c65b117fb68
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-12-28 15:31:53 -0700

    feat(core) Add query method
    * Provides abstraction layer over queries allowing third-party devs
    to contribute more advanced query processors

--- src/database/root.js
+++ src/database/root.js
@@ -122,10 +122,23 @@ class Database extends Graph {
 
   /**
    * description
+   * @param  {Mixed} query - query that is passed to the engine
+   * @param  {Object} options - options for executing the query
    * @return {undefined}
    */
-  query () {
-    throw new Error();
+  query (query, options) {
+
+    const config = this[settings];
+    const engines = config.engines;
+    const engine = options.engine;
+
+    // Make sure the engine exists
+    if (!engines.hasOwnProperty(engine)) {
+      throw new Error('No engine is defined in options');
+    }
+
+    const queryEngine = engines[engine];
+    return queryEngine.executeQuery(query, this);
   }
 }
 
--- src/database/test/query.test.js
+++ src/database/test/query.test.js
@@ -1,13 +1,23 @@
 /* eslint-env mocha */
 import Database from '../../index';
-import expect from 'expect';
+import expect, { spyOn } from 'expect';
+import { queryEngine } from './mocks';
 
 describe('A database query', () => {
 
   let db;
+  let executeQuery;
 
   beforeEach(() => {
-    db = new Database();
+    db = new Database({
+      engines: {
+        sql: queryEngine,
+      },
+    });
+    executeQuery = spyOn(queryEngine, 'executeQuery');
+  });
+  afterEach(() => {
+    executeQuery.restore();
   });
 
   it('should be a function', () => {
@@ -20,5 +30,25 @@ describe('A database query', () => {
     });
     expect(query).toThrow(Error);
   });
+
+  it('should call the correct query engine', () => {
+    const query = 'query';
+    const options = {
+      engine: 'sql',
+    };
+    db.query(query, options);
+    expect(executeQuery).toHaveBeenCalledWith(query, db);
+  });
+
+  it('should return correct value', () => {
+
+    executeQuery.andReturn('cheese');
+
+    const result = db.query(['users'], {
+      engine: 'sql',
+    });
+    expect(result).toEqual('cheese');
+  });
+
 
 });
