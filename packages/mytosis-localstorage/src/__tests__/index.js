20ce47eb58a0ab401e2a2a32a6798182700174ad
Jesse Gibson <Jesse_Gibson@me.com>
2017-06-03 21:36:59 -0600

    Add localStorage interface
    Reads and writes nodes.

---
+++ packages/mytosis-localstorage/src/__tests__/index.js
@@ -0,0 +1,80 @@
+import { Graph, Node } from 'graph-crdt';
+
+import LocalStoragePlugin from '../index';
+
+const createFakeStore = () => ({
+  removeItem() {},
+  setItem() {},
+  getItem() {},
+  clear() {},
+});
+
+describe('Mytosis LocalStorage', () => {
+  let store, graph, node;
+
+  beforeEach(() => {
+    graph = new Graph();
+    node = new Node();
+
+    graph.merge({ [node]: node });
+
+    const localStorage = global.localStorage;
+    localStorage.removeItem.mockReset();
+    localStorage.setItem.mockReset();
+    localStorage.getItem.mockReset();
+    localStorage.clear.mockReset();
+
+    store = new LocalStoragePlugin();
+  });
+
+  it('exports a function', () => {
+    expect(LocalStoragePlugin).toEqual(expect.any(Function));
+  });
+
+  it('sets the localStorage backend', () => {
+    const backend = createFakeStore();
+    const store = new LocalStoragePlugin({ backend });
+
+    expect(store.backend).toBe(backend);
+  });
+
+  it('defaults to use global localStorage', () => {
+    expect(store.backend).toBe(global.localStorage);
+  });
+
+  it('throws if the backend interface is invalid', () => {
+    const backend = {};
+    const create = () => new LocalStoragePlugin({ backend });
+
+    expect(create).toThrow();
+  });
+
+  describe('write()', () => {
+    it('writes every node in the graph', () => {
+      const other = new Node();
+      graph.merge({ [other]: other });
+      store.write({ graph });
+
+      expect(localStorage.setItem).toHaveBeenCalledWith(String(node), JSON.stringify(node));
+      expect(localStorage.setItem).toHaveBeenCalledWith(String(other), JSON.stringify(other));
+    });
+  });
+
+  describe('read()', () => {
+    it('returns null if nothing exists', () => {
+      const result = store.read(node.meta().uid);
+
+      expect(result).toBe(null);
+    });
+
+    it('returns the written item when it exists', () => {
+      localStorage.getItem.mockReturnValue(JSON.stringify(node));
+      const { uid } = node.meta();
+
+      const result = store.read({ key: uid });
+
+      expect(localStorage.getItem).toHaveBeenCalledWith(uid);
+      expect(result).toEqual(node.toJSON());
+    });
+  });
+});
---
+++ packages/mytosis-localstorage/src/mock-localstorage.js
@@ -0,0 +1,7 @@
+global.localStorage = {
+  toString: jest.fn(() => '[object Storage]'),
+  removeItem: jest.fn(),
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
