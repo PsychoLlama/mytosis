36e44fa1762862c23fcdf5c920c3883ec15f7cb6
Jesse Gibson <Jesse_Gibson@me.com>
2017-10-23 19:26:17 -0600

    Add derivation type
    Composes a single primitive into a new type. For instance, a date could
    be derived from a numeric timestamp, or a geographical coordinate could
    derive from a lat/long formatted string.
    
    The plan is to use derivations to represent pointers and dates.

---
+++ packages/m-types/src/Derivation.js
@@ -0,0 +1,71 @@
+// @flow
+import assert from 'minimalistic-assert';
+
+import type Primitive from './Primitive';
+
+type Definition = {
+  isValid(value: mixed): boolean,
+  dehydrate: Function,
+  hydrate: Function,
+};
+
+/**
+ * Constructs a new primitive from a serialized value.
+ */
+export default class Derivation {
+  _definition: Definition;
+  subtype: Primitive;
+  name: string;
+
+  /**
+   * @param  {String} name - A title for the new type.
+   * @param  {Primitive} primitive - Something to derive from.
+   * @param  {Object} def - The derivation rule set.
+   */
+  constructor(name: string, primitive: Primitive, def: Definition) {
+    assert(
+      /^[a-z][0-9a-z-]*$/.test(name),
+      `Invalid derivation name "${name}".`,
+    );
+
+    this.subtype = primitive;
+    this._definition = def;
+    this.name = name;
+  }
+
+  /**
+   * Takes a derived type and turns it into a primitive.
+   * Ensure the type is a valid derivation before attempting dehyrdation.
+   * @throws {Error} - If the dehydrator returns an invalid value.
+   * @param  {mixed} hydrated - Anything which makes it past the validator.
+   * @return {mixed} - Something belonging to the derived type.
+   */
+  dehydrate(hydrated: mixed) {
+    const value = this._definition.dehydrate(hydrated);
+
+    assert(
+      this.subtype.isValid(value),
+      `Serializer for type "${this.name}" returned an invalid value.`,
+    );
+
+    return value;
+  }
+
+  /**
+   * Takes a primitive value and turns it into the derived type.
+   * @param  {mixed} dehydrated - Something of the derived type.
+   * @return {mixed} - The hydrated value.
+   */
+  hydrate(dehydrated: mixed) {
+    return this._definition.hydrate(dehydrated);
+  }
+
+  /**
+   * Test whether the given value is a valid derivation instance.
+   * @param  {mixed} value - Anything but undefined.
+   * @return {Boolean} - Whether the value is valid.
+   */
+  isValid(value: mixed) {
+    return this._definition.isValid(value);
+  }
+}
---
+++ packages/m-types/src/__tests__/Derivation.test.js
@@ -0,0 +1,87 @@
+// @flow
+import Derivation from '../Derivation';
+import Primitive from '../Primitive';
+
+const number = new Primitive('number', {
+  isValid: value => typeof value === 'number',
+});
+
+const string = new Primitive('string', {
+  isValid: value => typeof value === 'string',
+});
+
+describe('Derivation', () => {
+  it('is a function', () => {
+    expect(Derivation).toEqual(expect.any(Function));
+  });
+
+  it('can dehydrate types', () => {
+    const time = new Derivation('time', number, {
+      dehydrate: date => date.getTime(),
+      hydrate: ms => new Date(ms),
+      isValid: () => true,
+    });
+
+    const date = new Date();
+    const dehydrated = time.dehydrate(date);
+
+    expect(dehydrated).toBe(date.getTime());
+  });
+
+  it('throws if the name is invalid', () => {
+    const id = value => value;
+    const def = { dehydrate: id, hydrate: id, isValid: () => true };
+
+    expect(() => new Derivation('Capital', number, def)).toThrow(/name/i);
+    expect(() => new Derivation('8-leading-number', number, def)).toThrow(
+      /name/i,
+    );
+    expect(() => new Derivation('-leading-hyphen', number, def)).toThrow(
+      /name/i,
+    );
+    expect(() => new Derivation('sym&ols', number, def)).toThrow(/name/i);
+    expect(() => new Derivation('spaced words', number, def)).toThrow(/name/i);
+    expect(() => new Derivation('', number, def)).toThrow(/name/i);
+
+    expect(() => new Derivation('lowercase', number, def)).not.toThrow();
+    expect(() => new Derivation('hyphen-ated', number, def)).not.toThrow();
+    expect(() => new Derivation('with-numb3r5', number, def)).not.toThrow();
+  });
+
+  it('can hydrate types', () => {
+    const coords = new Derivation('coords', string, {
+      dehydrate: coords => `${coords.lat},${coords.long}`,
+      isValid: () => true,
+      hydrate: map => {
+        const [lat, long] = map.split(',').map(Number);
+        return { lat, long };
+      },
+    });
+
+    expect(coords.hydrate('2,3')).toEqual({ lat: 2, long: 3 });
+  });
+
+  it('can indicate validity', () => {
+    const time = new Derivation('time', number, {
+      isValid: value => value instanceof Date,
+      dehydrate: date => date.getTime(),
+      hydrate: value => new Date(value),
+    });
+
+    expect(time.isValid(new Date())).toBe(true);
+    expect(time.isValid(5)).toBe(false);
+    expect(time.isValid('nope')).toBe(false);
+  });
+
+  it('throws if the dehydrator returns an incorrect type', () => {
+    const time = new Derivation('time', number, {
+      dehydrate: () => 'not a number',
+      hydrate: () => new Date(),
+      isValid: () => true,
+    });
+
+    const fail = () => time.dehydrate(new Date());
+
+    expect(fail).toThrow(/serializ(e|or)/i);
+  });
+});
