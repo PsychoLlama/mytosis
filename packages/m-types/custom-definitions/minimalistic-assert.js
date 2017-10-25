14668f3cb67e56ce0571da6c810fe67b4bd11fcb
Jesse Gibson <Jesse_Gibson@me.com>
2017-10-24 22:54:27 -0600

    Fix types
    I've been doing a bit more flow recently and learned a few tricks. I
    also learned I was being dumb by including minimalistic-assert in the
    custom-definitions folder.

--- packages/m-types/custom-definitions/minimalistic-assert.js
+++
@@ -1,6 +0,0 @@
-declare module 'minimalistic-assert' {
-  declare module.exports: {
-    (expression: mixed, message?: string): void,
-    equal(value1: mixed, value2: mixed, message?: string): void,
-  };
-}
