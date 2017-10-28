5c7c9f5b4b8bd5fc85b5c01e1b1d06c992b7bf99
Jesse Gibson <Jesse_Gibson@me.com>
2017-10-28 16:55:33 -0600

    Export migrations
    Exports some extra type utilities.

--- packages/types/src/index.js
+++ packages/types/src/index.js
@@ -2,4 +2,7 @@ export { default as Derivation } from './Derivation';
 export { default as Composite } from './Composite';
 export { default as Primitive } from './Primitive';
 export { default as Literal } from './Literal';
+export { default as Pointer } from './Pointer';
 export { default as Union } from './Union';
+
+export * as migrations from './migrations';
