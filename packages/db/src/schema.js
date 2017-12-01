9523f2ef6a49757b18da9e5fa27af54dce9ace32
Jesse Gibson <Jesse_Gibson@me.com>
2017-12-01 09:27:42 -0700

    Replace iterator utils with composite API
    Uses the new composite migration iterator instead of a few utility
    functions.

--- packages/db/src/schema.js
+++ packages/db/src/schema.js
@@ -5,24 +5,6 @@ type GraphEntryPoints = {
   [title: string]: Composite,
 };
 
-const getFirstVersion = (composite: Composite) => {
-  let ctx = composite;
-
-  while (ctx.lastVersion) {
-    ctx = ctx.lastVersion;
-  }
-
-  return ctx;
-};
-
-const everyVersionDo = (composite: Composite, fn: Composite => *) => {
-  let ctx = getFirstVersion(composite);
-  while (ctx) {
-    fn(ctx);
-    ctx = ctx.nextVersion;
-  }
-};
-
 /** Validates and indexes a collection of types. */
 export default class Schema {
   _types: Map<string, Composite> = new Map();
@@ -32,10 +14,10 @@ export default class Schema {
    */
   constructor(types: GraphEntryPoints) {
     const searchForTypes = (composite: Composite) => {
-      everyVersionDo(composite, version => {
-        const id = String(version);
-        this._types.set(id, version);
-      });
+      for (const type of Composite.toMigrationIterable(composite)) {
+        const id = String(type);
+        this._types.set(id, type);
+      }
 
       // Follow type pointers.
       if (composite.defaultType instanceof Pointer) {
