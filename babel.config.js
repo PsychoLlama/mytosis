4ec6a47f65e326f706371baf577f6e7812895ac5
Jesse Gibson <JesseTheGibson@gmail.com>
2019-01-03 14:19:17 -0700

    Upgrade freighter for babel 7
    I'm kinda procrastinating doing proper work in this project. I'm tired
    of Flow and before I add much more code, I want to see if Typescript is
    any better. Babel 7 introduced better support for it, so I might give it
    a shot.
    
    Right now all the tests are failing, but hey, the projects successfully
    compile.

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+/* eslint-env node */
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: [require('@freighter/scripts/babel-preset')],
+    plugins: [
+      require('@babel/plugin-proposal-async-generator-functions'),
+      require('@babel/plugin-proposal-export-namespace-from'),
+      require('@babel/plugin-proposal-object-rest-spread'),
+      require('@babel/plugin-proposal-class-properties'),
+      require('@babel/plugin-transform-runtime'),
+    ],
+  };
+};
